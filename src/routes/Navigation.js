905cffcad8c26776db8054100ae8ed7ed9566900
unknown <qkrcksdna@naver.com>
2021-02-17 12:01:42 +0900

    Home, About 버튼 수정

--- src/routes/Navigation.js
+++ src/routes/Navigation.js
@@ -5,7 +5,7 @@ import "./Navigation.css"
 //a href 대신 Link to 를 사용
 export default function Navigation() {
     return (
-        <div>
+        <div className="nav">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
