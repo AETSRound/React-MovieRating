8f96406f89d9c6065d59b70c4f15517a4820c286
tonypark-cw <qkrcksdna@naver.com>
2021-02-18 17:58:43 +0900

    About 페이지 추가

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,7 +5,17 @@ export default function About(props) {
     console.log(props);
     return (
         <div>
-            hello
+            <p className="about__container">
+                이 사이트는 개발용 입니다.<br/>
+                Nomad Coder Nicolas의 Movie Rating Web <br/>
+                강의를 통한 클론 코딩 결과입니다.
+                <span>
+                    <span>개발자 :  TonyPark</span>
+                    <span>GIT : <a href="http://github.com/tonypark-cw">깃허브</a></span>
+                </span>
+            </p>
+
+
         </div>
     )
 }
