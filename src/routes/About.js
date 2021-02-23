15a73aa4491f0c6d361826adeb06d89676ebd27f
둥글 <doongle@dung-geul-ui-MacBookPro.local>
2021-02-23 10:27:01 +0900

    다른 페이지 링크

--- src/routes/About.js
+++ src/routes/About.js
@@ -11,8 +11,10 @@ export default function About(props) {
                 강의를 통한 클론 코딩 결과입니다.
                 <span>
                     <span>개발자 :  TonyPark</span>
-                    <span>GIT : <a href="http://github.com/tonypark-cw">깃허브</a></span>
+                    <span><a href="http://github.com/tonypark-cw">깃허브</a></span>
+                    <span>다른 페이지 1 : <a href="https://tonypark-cw.github.io/todochrome/">VanillaJS로 만든 TODO앱</a></span>
                 </span>
+
             </p>
 
 
