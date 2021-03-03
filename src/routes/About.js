e8788ceff4f8b2d0e18b4090d55d967cc750ae22
tonypark-cw <qkrcksdna@naver.com>
2021-03-03 14:55:51 +0900

    anchor decoration

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,18 +6,18 @@ export default function About(props) {
     return (
         <div>
             <p className="about__container">
+                <a href="http://github.com/tonypark-cw"><img alt="ToRootGit" className="root_git" src="http://localhost:3000/react-movierating/Octocat.jpg"></img></a>
                 이 사이트는 개발용 입니다.<br/>
                 Nomad Coder Nicolas의 Movie Rating Web <br/>
-                강의를 통한 클론 코딩 결과입니다.
+                강의를 통한 클론 코딩 페이지입니다.
                 <span>
                     <span>개발자 :  TonyPark</span>
-                    <span><a href="http://github.com/tonypark-cw">깃허브</a></span>
-                    <span>다른 페이지 1 : <a href="https://tonypark-cw.github.io/todochrome/">VanillaJS로 만든 TODO앱</a></span>
+                    <span><a href="http://github.com/tonypark-cw/react-movierating"><img className="git_Icon" alt="ToMovieRatingGit" src="http://localhost:3000/react-movierating/GitHub-Mark-32px.png"></img></a></span><br/>
+                    <span><a href="https://tonypark-cw.github.io/todochrome/">🎈VanillaJS로 만든 TODO앱</a></span><br/>
+                    <span><a href="https://tonypark-cw.github.io/painterjs/">🎈VanillaJS로 만든 Painter</a></span><br/>
+                    <span><a href="https://tonypark-cw.github.io/goyange/">🎈고양이 이미지 검색 페이지</a></span><br/>
                 </span>
-
             </p>
-
-
         </div>
     )
 }
